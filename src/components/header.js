e71843a9367394042f13845510a4df3f4e5fadd4
itsanan <valaddict007@gmail.com>
2019-07-08 20:07:30 +0700

    Finished the navbar section, with some animations.

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,20 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Logo from "../images/meezu-logo.svg"
+import SearchIcon from "../images/search-icon.svg"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <div>
-    
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link className="Logo" to="/"><img src={Logo} width="30"></img></Link>
+      <Link to="/news">News</Link>
+      <Link to="/featutes">Features</Link>
+      <Link to="/prices">Prices</Link>
+      <Link to="/blog">Blog</Link>
+      <Link className="SearchIcon" to="/"><img src={SearchIcon} width="30"></img></Link>
+    </div>
   </div>
 )
 
